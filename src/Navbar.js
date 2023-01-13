9b9a08dbff6748780970f4a6481f3a4fd452d7a7
Saloni Maheshwari <96430514+salonimaheshwarii@users.noreply.github.com>
2023-01-13 21:58:50 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar () {
+  return (
+      <div className='Nav'>
+        <a href="">Home</a>
+        <a href="">About us</a>
+        <a href="">Team</a>
+        <a href="">Events</a>
+        <a href="">Gallery</a>
+        <a href="">Contact us</a>
+    </div>
+  )
+}
+
+export default Navbar
